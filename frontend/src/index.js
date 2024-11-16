d7fe2a8565028a7f3c743646cb32953f57c5d5f7
Sahil Rastogi <149312261+rastogi20@users.noreply.github.com>
2024-11-16 13:53:21 +0530

    Add files via upload

---
+++ frontend/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import "react-day-picker/style.css"
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
